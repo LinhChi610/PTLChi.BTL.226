9a9e4d1cd01861449b1ab9b81d6c1b1fcf7f6ceb
ADMIN <ADMIN@DESKTOP-JE7MIHI>
2022-05-06 10:34:53 +0700

    up6

---
+++ Migrations/202205060256241_Delete Table3.cs
@@ -0,0 +1,18 @@
+﻿namespace PTLChi.BTL._226.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteTable3 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Don_dh", "ID_tinhtrang");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Don_dh", "ID_tinhtrang", c => c.Int(nullable: false));
+        }
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,12 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [ID_HoaDon]
+      ,[ID_KhachHang]
+     
+      ,[Id_NhanvienGH]
+      ,[Ngay_lap]
+      ,[Bao_hanh]
+      ,[Gia_khuyen_mai]
+      ,[Tong_Gia]
+      ,[Dia_Chi_Nhan]
+      ,[Chi_chu]
+  FROM [BTL2].[dbo].[Don_dh]
